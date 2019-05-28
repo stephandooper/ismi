8495b52504821b3896417c814a2df975522819f4
Tristan de Boer <info@tristandeboer.nl>
2019-05-28 14:20:42 +0200

    Added predictions

--- Project/recnn_run.py
+++ Project/recnn_run.py
@@ -4,5 +4,6 @@ config = {'model': 'recnn',
                'epochs': 10,
                'batch_size': 32,
                'target_size':(96,96),
-               'only_use_subset':True}
+               'only_use_subset':False}
+
 run_experiment(config, predict_test="recnn_no_augmentation", predict_val="recnn_no_augmentation")
